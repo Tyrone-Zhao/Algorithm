a3da0528b6ad68b5dc4e593ca16bf45676d2270b
tyrone-zhao <tyrone-zhao@qq.com>
2019-07-24 20:32:39 +0800

    增加归并排序和最长连续的单个字符

--- dailyAlgorithm.py
+++ dailyAlgorithm.py
@@ -3893,6 +3893,70 @@ def main():
     print(f"对应的二叉树共有{len(root_list)}棵,它们均为二叉搜索树")
 
 
+def merge_sort(li):
+    """
+        归并排序
+    :param li:
+    :return:
+    """
+    # 不断递归调用自己一直到拆分成成单个元素的时候就返回这个元素，不再拆分了
+    if len(li) <= 1:
+        return li
+
+    # 取拆分的中间位置
+    mid = len(li) // 2
+    # 拆分过后左右两侧子串
+    left = li[:mid]
+    right = li[mid:]
+
+    # 对拆分过后的左右再拆分 一直到只有一个元素为止
+    # 最后一次递归时候ll和lr都会接到一个元素的列表
+    # 最后一次递归之前的ll和rl会接收到排好序的子序列
+    ll = merge_sort(left)
+    rl = merge_sort(right)
+
+    # 我们对返回的两个拆分结果进行排序后合并再返回正确顺序的子列表
+    # 这里我们调用拎一个函数帮助我们按顺序合并ll和lr
+    return merge(ll, rl)
+
+
+# 这里接收两个列表
+def merge(left, right):
+    # 从两个有顺序的列表里边依次取数据比较后放入result
+    # 每次我们分别拿出两个列表中最小的数比较，把较小的放入result
+    result = []
+    while len(left) > 0 and len(right) > 0:
+        # 为了保持稳定性，当遇到相等的时候优先把左侧的数放进结果列表，因为left本来也是大数列中比较靠左的
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    # while循环出来之后 说明其中一个数组没有数据了，我们把另一个数组添加到结果数组后面
+    result += left
+    result += right
+    return result
+
+
+def maxLongCharacters(string):
+    """
+        找出字符中最长连续的单个字符
+        比如str = 'qqqeeedsdsdsfsdfsdfsdfsdfaaaaaaaaaaaaaaaaaaaaaaaaqqqqq'
+        那么最长的连续单个字符就是: aaaaaaaaaaaaaaaaaaaaaaaa
+    """
+    matrix = [[""], [0]]
+    for s in string:
+        if s == matrix[0][-1]:
+            matrix[1][-1] += 1
+        else:
+            matrix[0].append(s)
+            matrix[1].append(1)
+
+    max_num = max(matrix[1])
+    i = matrix[1].index(max_num)
+
+    return matrix[0][i], max_num
+
+
 main()
 
 import time
